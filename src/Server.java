b399f781734c764ed53f3d190e2d841070a9e7dd
Melle Dijkstra <melle210202@gmail.com>
2018-01-08 23:26:29 +0100

    [TASK] Correct XML parsing from generator and created Measurement object which we can work with

--- src/Server.java
+++ src/Server.java
@@ -1,19 +1,17 @@
-/**
- * Created by jandu on 23/12/2017.
- */
-
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-    private int portNumber = 3301;
 
     public void run() {
         try {
-            ServerSocket server = new ServerSocket(this.portNumber);
+            ServerSocket server = new ServerSocket(3301);
             System.out.print("Server started...");
 
-            while(true) {
+            while (true) {
                 Socket s = server.accept();
                 new SocketThread(s).start();
             }
@@ -35,13 +33,28 @@ class SocketThread extends Thread {
     public void run() {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            while (s.isConnected() && !this.isInterrupted()) {
-                String msg = in.readLine();
-                if(msg == null) break;
-                System.out.println(msg);
+            StringBuilder xml = new StringBuilder();
+            while (s.isConnected() && !isInterrupted()) {
+                String line = in.readLine();
+                if (line != null) {
+                    xml.append(line);
+                    if (xml.toString().endsWith("</WEATHERDATA>")) {
+                        // processing of message is done in same thread, reading will have to wait
+                        processMessage(xml.toString());
+                        xml.setLength(0);
+                    }
+                } else {
+                    in.close();
+                    s.close();
+                    break;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void processMessage(String xml) {
+        Parser parser = new Parser(xml);
     }
 }
