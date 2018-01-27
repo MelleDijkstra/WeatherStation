92346ca8b9ab442abc5dbf4133d49454ac09f43c
Melle Dijkstra <melle210202@gmail.com>
2018-01-27 20:53:24 +0100

    Refactored auto discovery

--- DataManager/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        // search for service on network
-        Discoverer d = new Discoverer();
-        d.discover();
-    }
-
-}
