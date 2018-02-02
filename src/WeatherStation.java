c4ab701868a716d8a7d64e20c7f311b706038042
Melle Dijkstra <melle210202@gmail.com>
2018-02-02 20:19:22 +0100

    Use blocking queue

--- src/WeatherStation.java
+++ src/WeatherStation.java
@@ -3,38 +3,43 @@ import models.Measurement;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class WeatherStation {
 
     private Server s;
     //VMConnection vmc;
     //CorrectionModel cm;
-    private ConcurrentLinkedQueue<String> queue;
+    private LinkedBlockingQueue<String> queue;
 
     WeatherStation() {
         // TODO: setup server and autodiscovery
         // Making a ThreadSafe Queue
-        queue = new ConcurrentLinkedQueue<>();
+        queue = new LinkedBlockingQueue<>();
         s = new Server(queue);
     }
 
     public void run() {
         // Start the server
         s.start();
-        while(true) {
-            // Constantly check if new XML messages are coming in
-            System.out.println("Polling...");
-            String xml = queue.poll();
-            if(xml != null) {
-                // If there is an XML message, parse it. Parser returns Measurement objects from the XML
-                List<Measurement> measurementList = Parser.parseFromXML(xml);
-                if(measurementList != null) {
-                    System.out.println("\nTotal: "+measurementList.size()+"\n");
-                    for (Measurement measurement : measurementList) {
-                        System.out.println(measurement);
+        while (true) {
+            try {
+                // Constantly check if new XML messages are coming in
+                System.out.println("Polling...");
+                String xml = queue.take();
+                if (xml != null) {
+                    // If there is an XML message, parse it. Parser returns Measurement objects from the XML
+                    List<Measurement> measurementList = Parser.parseFromXML(xml);
+                    if (measurementList != null) {
+                        System.out.println("\nTotal: " + measurementList.size() + "\n");
+                        for (Measurement measurement : measurementList) {
+                            System.out.println(measurement);
+                        }
                     }
+                    //List<Measurement> correctedMeasurements = cm.runCorrections(measurementList);
                 }
-                //List<Measurement> correctedMeasurements = cm.runCorrections(measurementList);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
