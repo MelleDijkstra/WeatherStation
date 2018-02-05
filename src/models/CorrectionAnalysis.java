da07b4a1edffb8a56dcaac570c83223038a26db6
ThijsMartena <t.j.martena@st.hanze.nl>
2018-02-05 09:35:29 +0100

    correction when field is empty. if there is no data available for correction it deletes the measurement.

--- src/models/CorrectionAnalysis.java
+++ src/models/CorrectionAnalysis.java
@@ -5,11 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CorrectionAnalysis {
-
+    Measurement measurement;
     private FixedQueue<Measurement> measurementHistory;
 
     public CorrectionAnalysis() {
         measurementHistory = new FixedQueue<>(30);
+        measurement = new Measurement();
     }
 
     /**
@@ -18,9 +19,13 @@ public class CorrectionAnalysis {
      * @return The corrected measurement
      */
     public Measurement correct(Measurement measurement) {
-
         for (HashMap.Entry<Measurement.Fields, Float> entry : measurement.fields.entrySet()) {
-            if (entry.getValue() == null) {
+            // check if value is null and if there is data to make a correction
+            if (entry.getValue() == null && measurementHistory.isEmpty()){
+                // remove value because no data yet to make a correction
+                measurement.fields.remove(entry.getKey());
+            // check if value is null or if value has too much deviation
+            } else if (entry.getValue() == null) {
                 Float tot = 0f;
                 for (Measurement m : measurementHistory) {
                     tot += m.fields.get(entry.getKey());
@@ -29,7 +34,6 @@ public class CorrectionAnalysis {
                 measurement.fields.put(entry.getKey(), avg);
             }
         }
-
         return measurement;
     }
 
